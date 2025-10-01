46a5fa59ba274b63575956533c0b54bd3ae14b28
dasha-novo <dasanovo4127@gmail.com>
2025-10-01 16:09:09 +0300

    bugFix

--- multi_before_and_after_negative.c
+++ multi_before_and_after_negative.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include "multi_before_and_after_negative.h"
+#include "index_first_negative.h"
+#include "index_last_negative.h"
 int multi_before_and_after_negative(int *arr, int size){
     int start = index_first_negative(arr, size);
     int end = index_last_negative(arr, size);
--- multi_between_negative.c
+++ multi_between_negative.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "multi_between_negative.h"
+#include "index_first_negative.h"
+#include "index_last_negative.h"
+
 int multi_between_negative(int *arr, int size){
     int start = index_first_negative(arr, size);
     int end = index_last_negative(arr, size);
